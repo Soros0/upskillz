0059e3005f3469ef3d9055488f641ca48d73994f
Soros0 <140617613+Soros0@users.noreply.github.com>
2025-01-26 18:22:40 +0100

    servers.js

---
+++ backend/servers.js
@@ -0,0 +1,8 @@
+// server.js
+const mongoose = require("mongoose");
+const config = require("./config");
+
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
