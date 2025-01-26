f5a4358df188de431c986d094e8400eee53552c4
Soros0 <140617613+Soros0@users.noreply.github.com>
2025-01-27 00:39:10 +0100

    admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const QuizResponse = require("../models/QuizResponse");
+const authMiddleware = require("../authMiddleware");
 
+router.get("/users", authMiddleware, async (req, res) => {
+  const users = await User.find().populate("quizResponses");
+  res.json(users);
+});
+
+module.exports = router;
