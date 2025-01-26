9e1293875777e1f4ecfbca0f660a7f6eb5c756cb
Soros0 <140617613+Soros0@users.noreply.github.com>
2025-01-27 00:37:39 +0100

    QuizResponse.js

--- backend/models/QuizResponse.js
+++ backend/models/QuizResponse.js
@@ -1,1 +1,10 @@
 
+const mongoose = require("mongoose");
+
+const quizResponseSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  responses: { type: Array, required: true },
+  personalityType: { type: String, required: true },
+});
+
+module.exports = mongoose.model("QuizResponse", quizResponseSchema);
